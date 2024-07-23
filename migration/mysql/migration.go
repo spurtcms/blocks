621c598acfe195cca813a85e2635e080872f9ee0
menaka-piccotalent <menaka@piccotalent.com>
2024-07-23 13:24:48 +0530

    struct changes

--- migration/mysql/migration.go
+++ migration/mysql/migration.go
@@ -8,8 +8,10 @@ import (
 
 type TblBlocks struct {
 	Id          int       `gorm:"primaryKey;auto_increment;"`
-	BlogTitle   string    `gorm:"type:varchar(255)"`
+	BlockTitle  string    `gorm:"type:varchar(255)"`
 	Description string    `gorm:"type:varchar(255)"`
+	ImagePath   string    `gorm:"type:varchar(255)"`
+	BlockView   string    `gorm:"type:varchar(900)"`
 	CreatedOn   time.Time `gorm:"type:datetime;DEFAULT:NULL"`
 	CreatedBy   int       `gorm:"type:int"`
 	ModifiedOn  time.Time `gorm:"DEFAULT:NULL"`
@@ -17,8 +19,6 @@ type TblBlocks struct {
 	DeletedOn   time.Time `gorm:"type:datetime;DEFAULT:NULL"`
 	DeletedBy   int       `gorm:"type:integer;DEFAULT:NULL"`
 	IsDeleted   int       `gorm:"type:int;DEFAULT:0"`
-	ImagePath   string    `gorm:"type:varchar(255)"`
-	BlocksView  string    `gorm:"type:varchar(600)"`
 }
 
 func MigrationTables(db *gorm.DB) {
--- migration/postgres/migration.go
+++ migration/postgres/migration.go
@@ -8,8 +8,10 @@ import (
 
 type TblBlocks struct {
 	Id          int       `gorm:"primaryKey;auto_increment;type:serial"`
-	BlogTitle   string    `gorm:"type:character varying"`
-	Description string    `gorm:"type:character varying"`
+	BlockTitle  string    `gorm:"type:character varying"`
+	Description string    `gorm:"type:text"`
+	ImagePath   string    `gorm:"type:character varying"`
+	BlockView   string    `gorm:"type:text"`
 	CreatedOn   time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
 	CreatedBy   int       `gorm:"type:int"`
 	ModifiedOn  time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
@@ -17,8 +19,6 @@ type TblBlocks struct {
 	DeletedOn   time.Time `gorm:"type:timestamp without time zone;DEFAULT:NULL"`
 	DeletedBy   int       `gorm:"type:integer;DEFAULT:NULL"`
 	IsDeleted   int       `gorm:"type:integer;DEFAULT:0"`
-	ImagePath   string    `gorm:"type:character varying"`
-	BlocksView  string    `gorm:"type:character varying"`
 }
 
 func MigrationTables(db *gorm.DB) {
